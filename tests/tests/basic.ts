16d545ab842ef555270d77b6659e73ce99a8862c
Carifio24 <carifio24@gmail.com>
2026-01-14 16:51:13 -0500

    Update tests to use Percy screenshot functionality.

--- tests/tests/basic.ts
+++ tests/tests/basic.ts
@@ -8,6 +8,9 @@ import {
 
 import { assert } from "chai";
 
+import type { WebDriver } from "selenium-webdriver";
+import { percyScreenshot } from "@percy/selenium-webdriver";
+
 import {
   expectAllNotPresent,
   expectAllVisible,
@@ -24,98 +27,114 @@ const tests: GreenCometTests = {
     browser.globals.waitForConditionTimeout = 30000;
     this.app = browser.page.GreenComet();
     this.sections = this.app.section as GreenCometSections;
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error The `driver` member is defined
+    this.driver = browser.driver;
   },
 
   'Navigation and loading': function() {
     this.app.navigate().waitForReady();
   },
 
-  'Initial configuration': function() {
-    app.expect.title().to.equal(this.app.props.title);
-    expectAllVisible(this.app, [
+  'Initial configuration': async function() {
+
+    await percyScreenshot(this.driver, "Initial configuration");
+
+    await app.expect.title().to.equal(this.app.props.title);
+    await expectAllVisible(this.app, [
       "@splashScreen",
       "@splashClose"
     ]);
-    expectAllNotPresent(this.app, [
+    await expectAllNotPresent(this.app, [
       "@videoDialog",
       "@locationDialog",
       "@infoSheet"
     ]);
 
-    this.app.click("@splashClose");
-    expectAllNotPresent(this.app, [
+    await this.app.click("@splashClose");
+    await expectAllNotPresent(this.app, [
       "@splashScreen",
       "@splashClose"
     ]);
 
-    expectAllVisible(this.sections.topContent, [
+    await expectAllVisible(this.sections.topContent, [
       "@videoIcon",
       "@textIcon",
       "@mapIcon"
     ]);
 
     const bottomContent = this.sections.bottomContent;
-    expectAllVisible(bottomContent, [
+    await expectAllVisible(bottomContent, [
       "@playPauseIcon",
       "@slider"
     ]);
 
-    bottomContent.expect.elements("@creditIcon").count.to.equal(bottomContent.props.creditIconCount);
+    await bottomContent.expect.elements("@creditIcon").count.to.equal(bottomContent.props.creditIconCount);
     
     const folderView = this.sections.folderView;
-    folderView.expect.elements("@folderItem").count.to.equal(folderView.props.folderImageCount);
-    folderView.expect.element("@expandHeader").text.to.match(folderView.props.expandedHeaderText);
-    folderView.expect.element("@expandChevron").to.have.attribute("data-icon", "chevron-up");
+    await folderView.expect.elements("@folderItem").count.to.equal(folderView.props.folderImageCount);
+    await folderView.expect.element("@expandHeader").text.to.match(folderView.props.expandedHeaderText);
+    await folderView.expect.element("@expandChevron").to.have.attribute("data-icon", "chevron-up");
 
     const controls = this.sections.controls;
-    controls.expect.element("@openCloseButton").to.have.attribute("data-icon", "chevron-down");
-    controls.expect.element("@gridInput").to.be.selected;
-    controls.expect.element("@constellationsInput").to.not.be.selected;
-    controls.expect.element("@horizonInput").to.not.be.selected; 
+    await controls.expect.element("@openCloseButton").to.have.attribute("data-icon", "chevron-down");
+    await controls.expect.element("@gridInput").to.be.selected;
+    await controls.expect.element("@constellationsInput").to.not.be.selected;
+    await controls.expect.element("@horizonInput").to.not.be.selected; 
 
-    controls.expect.element("@selectedLocationTimeLabel").text.to.match(controls.props.selectedLocationTimeText);
-    controls.expect.element("@centerOnNowButtonContent").text.to.match(controls.props.centerOnNowText);
-    controls.expect.element("@playCometImagesContent").text.to.match(controls.props.playCometImagesText);
+    await controls.expect.element("@selectedLocationTimeLabel").text.to.match(controls.props.selectedLocationTimeText);
+    await controls.expect.element("@centerOnNowButtonContent").text.to.match(controls.props.centerOnNowText);
+    await controls.expect.element("@playCometImagesContent").text.to.match(controls.props.playCometImagesText);
 
-    expectAllVisible(controls, [
+    await expectAllVisible(controls, [
       "@topRow", "@openCloseButton",
       "@gridCheckbox", "@constellationsCheckbox", "@horizonCheckbox",
       "@selectedLocationTimeLabel", "@selectedLocationTimeInput",
       "@timeIcon", "@centerOnNowButton", "@playCometImagesButton"
     ]);
 
-    controls.click("@openCloseButton");
+    await percyScreenshot(this.driver, "Controls open");
+
+    await controls.click("@openCloseButton");
+
+    await percyScreenshot(this.driver, "Controls closed");
   },
 
-  'Open video': function() {
-    this.sections.topContent.click("@videoIcon");
-    this.app.expect.element("@videoDialog").to.be.visible;
+  'Open video': async function() {
+    await this.sections.topContent.click("@videoIcon");
+    await this.app.expect.element("@videoDialog").to.be.visible;
     expectAllVisible(this.sections.videoDialog, [
       "@video", "@closeIcon"
     ]);
 
-    this.sections.videoDialog.click("@closeIcon");
-    this.app.expect.element("@videoDialog").to.not.be.present;
+    await percyScreenshot(this.driver, "Video open");
+
+    await this.sections.videoDialog.click("@closeIcon");
+    await this.app.expect.element("@videoDialog").to.not.be.present;
+
+    await percyScreenshot(this.driver, "Video closed");
   },
 
-  'Info text': function(browser: NightwatchBrowser) {
-    this.sections.topContent.click("@textIcon");
-    this.app.expect.element("@infoSheet").to.be.visible;
+  'Info text': async function(browser: NightwatchBrowser) {
+    await this.sections.topContent.click("@textIcon");
+    await this.app.expect.element("@infoSheet").to.be.visible;
 
     const infoSheet = this.sections.infoSheet;
-    expectAllVisible(infoSheet, [
+    await expectAllVisible(infoSheet, [
       "@closeIcon",
       "@infoTabHeader",
       "@wwtTabHeader",
       "@infoText"
     ]);
-    infoSheet.expect.element("@wwtText").to.not.be.present;
-    infoSheet.expect.elements("@tabHeader").count.to.equal(infoSheet.props.tabCount);
+    await infoSheet.expect.element("@wwtText").to.not.be.present;
+    await infoSheet.expect.elements("@tabHeader").count.to.equal(infoSheet.props.tabCount);
+
+    await percyScreenshot(this.driver, "Info sheet open");
     
     // getWindowSize seems to return the height of the browser
     // (that is, including the tab and address bars)
     // but we want just the height of the viewport
-    this.app.getElementSize("html", (windowSize) => {
+    await this.app.getElementSize("html", (windowSize) => {
       this.app.getElementSize("@mainContent", (mainContentSize) => {
         const wsize = windowSize.value as WindowSize;
         const csize = mainContentSize.value as WindowSize;
@@ -124,69 +143,95 @@ const tests: GreenCometTests = {
       });
     });
 
-    infoSheet.click("@wwtTabHeader");
-    infoSheet.expect.element("@infoText").to.be.present;
-    infoSheet.expect.element("@infoText").to.not.be.visible;
+    await infoSheet.click("@wwtTabHeader");
+    await infoSheet.expect.element("@infoText").to.be.present;
+    await infoSheet.expect.element("@infoText").to.not.be.visible;
 
-    infoSheet.click("@infoTabHeader");
-    infoSheet.expect.element("@wwtText").to.be.present;
-    infoSheet.expect.element("@wwtText").to.not.be.visible;
+    await percyScreenshot(this.driver, "Info sheet - WWT Tab");
 
-    infoSheet.click("@closeIcon");
-    this.app.expect.element("@infoSheet").to.not.be.present;
+    await infoSheet.click("@infoTabHeader");
+    await infoSheet.expect.element("@wwtText").to.be.present;
+    await infoSheet.expect.element("@wwtText").to.not.be.visible;
+
+    await percyScreenshot(this.driver, "Info sheet - Info Tab");
+
+    await infoSheet.click("@closeIcon");
+    await this.app.expect.element("@infoSheet").to.not.be.present;
+
+    await percyScreenshot(this.driver, "Info sheet closed");
   },
 
-  'Location selector': function() {
-    this.sections.topContent.click("@mapIcon");
+  'Location selector': async function() {
+    await this.sections.topContent.click("@mapIcon");
+
+    await percyScreenshot(this.driver, "Location selector open");
   
     const locationDialog = this.sections.locationDialog;
-    expectAllVisible(locationDialog, [
+    await expectAllVisible(locationDialog, [
       "@useMyLocationButton",
       "@mapContainer"
     ]);
-    locationDialog.expect.element("@useMyLocationButtonContent").text.to.match(locationDialog.props.useMyLocationText);
-    locationDialog.expect.element("@actionText").text.to.match(locationDialog.props.actionText);
+    await locationDialog.expect.element("@useMyLocationButtonContent").text.to.match(locationDialog.props.useMyLocationText);
+    await locationDialog.expect.element("@actionText").text.to.match(locationDialog.props.actionText);
 
-    browser.sendKeys("html", browser.Keys.ESCAPE); 
-    this.app.expect.element("@locationDialog").to.not.be.present;
+    await browser.sendKeys("html", browser.Keys.ESCAPE); 
+    await this.app.expect.element("@locationDialog").to.not.be.present;
+
+    await percyScreenshot(this.driver, "Location selector closed");
   },
 
-  'Folder View': function() {
+  'Folder View': async function() {
     const folderView = this.sections.folderView;
-    folderView.click("@expandRow");
-    folderView.expect.elements("@folderItem").count.to.equal(0);
-    folderView.expect.element("@expandHeader").text.to.match(folderView.props.contractedHeaderText);
-    folderView.expect.element("@expandChevron").to.have.attribute("data-icon", "chevron-down");
+    await folderView.click("@expandRow");
+    await folderView.expect.elements("@folderItem").count.to.equal(0);
+    await folderView.expect.element("@expandHeader").text.to.match(folderView.props.contractedHeaderText);
+    await folderView.expect.element("@expandChevron").to.have.attribute("data-icon", "chevron-down");
 
-    folderView.click("@expandRow");
-    folderView.expect.elements("@folderItem").count.to.equal(folderView.props.folderImageCount);
-    folderView.expect.element("@expandHeader").text.to.match(folderView.props.expandedHeaderText);
-    folderView.expect.element("@expandChevron").to.have.attribute("data-icon", "chevron-up");
+    await percyScreenshot(this.driver, "Folder view open");
+
+    await folderView.click("@expandRow");
+    await folderView.expect.elements("@folderItem").count.to.equal(folderView.props.folderImageCount);
+    await folderView.expect.element("@expandHeader").text.to.match(folderView.props.expandedHeaderText);
+    await folderView.expect.element("@expandChevron").to.have.attribute("data-icon", "chevron-up");
+
+    await percyScreenshot(this.driver, "Folder view closed");
   },
 
-  'Control Panel': function() {
+  'Control Panel': async function() {
     const controls = this.sections.controls;
 
-    controls.expect.element("@openCloseButton").to.have.attribute("data-icon", "gear");
-    expectAllNotPresent(controls, [
+    await controls.expect.element("@openCloseButton").to.have.attribute("data-icon", "gear");
+    await expectAllNotPresent(controls, [
       "@gridInput", "@constellationsInput", "@horizonInput",
       "@selectedLocationTimeLabel", "@selectedLocationTimeInput",
       "@timeIcon", "@centerOnNowButton", "@playCometImagesButton"
     ]);
 
-    controls.click("@openCloseButton");
-    controls.expect.element("@openCloseButton").to.have.attribute("data-icon", "chevron-down");
-    expectAllVisible(controls, [
+    await controls.click("@openCloseButton");
+    await controls.expect.element("@openCloseButton").to.have.attribute("data-icon", "chevron-down");
+    await expectAllVisible(controls, [
       "@topRow", "@openCloseButton",
       "@gridCheckbox", "@constellationsCheckbox", "@horizonCheckbox",
       "@selectedLocationTimeLabel", "@selectedLocationTimeInput",
       "@timeIcon", "@centerOnNowButton", "@playCometImagesButton"
     ]);
-    
+
+    await percyScreenshot(this.driver, "Control panel open");
+
+    await controls.click("@openCloseButton");
+    await controls.expect.element("@openCloseButton").to.have.attribute("data-icon", "chevron-down");
+    await expectAllNotPresent(controls, [
+      "@topRow", "@openCloseButton",
+      "@gridCheckbox", "@constellationsCheckbox", "@horizonCheckbox",
+      "@selectedLocationTimeLabel", "@selectedLocationTimeInput",
+      "@timeIcon", "@centerOnNowButton", "@playCometImagesButton"
+    ]);
+
+    await percyScreenshot(this.driver, "Control panel closed");
   },
 
-  after: function(browser: NightwatchBrowser) {
-    browser.end();
+  after: async function(browser: NightwatchBrowser) {
+    await browser.end();
   }
 };
 
